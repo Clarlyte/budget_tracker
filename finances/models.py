b1e617e3bdf472f688fe6a4675a1b64f83fcb63a
turks223-freddss <marfred.deen02@gmail.com>
2025-04-27 05:24:27 +0800

    added inital code fro finance

--- finances/models.py
+++ finances/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 from django.conf import settings
 
-#add models here
+class Income(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    income = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.income} on {self.date}"
+
+class Expense(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    expense = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField()
+    description = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category} - {self.expense} on {self.date}"
--- finances/urls.py
+++ finances/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 #add urls here
 
-urlpatterns = []  # Empty list since we removed Entry views
+app_name = 'finances'
+
+urlpatterns = [
+    path('expenses/', views.ExpenseListView.as_view(), name='expense_list'),  
+    path('income/add/', views.IncomeCreateView.as_view(), name='income_add'), 
+    path('expense/add/', views.ExpenseCreateView.as_view(), name='expense_add'),  
+    path('expense/<int:pk>/delete/', views.ExpenseDeleteView.as_view(), name='expense_delete'),
+]
